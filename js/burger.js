17a558e9bc20de2bdbf62f212cd307a826c8297b
Vova <he.beite.pliz@gmail.com>
2022-10-19 18:31:36 +0300

    HouseVOP
    HouseVOP

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.nav__burger').click(function(e){
+        $('.nav__burger, .nav__menu').toggleClass('active')
+        $('body').toggleClass('lock')
+    })
+});
